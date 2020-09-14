c7cd144325ae132d1ca1bed1401b89351699f77e
pranav khapra <pranavkhapra23@gmail.com>
2020-09-14 15:46:56 +0530

    some errors fixed

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,39 +1,74 @@
 import React, { useState,useEffect } from 'react'
 import axios from 'axios'
 function Search() {
-    const [searchText,setSearchText]= useState('user')
+    const [searchText,setSearchText]= useState('default ')
+    const [debouncedText,setDebouncedText]=useState(searchText)
     const [results,setResults]=useState([])
+
+     
     useEffect(()=>{
+        const timerId=setTimeout(()=>{
+            setDebouncedText(searchText)
+        },1000)
+
+
+        return ()=>{
+            clearTimeout(timerId)
+        }
+    },[searchText])
+      
+     useEffect(()=>{
         const search = async ()=>{
-     const response = await axios.get('https://en.wikipedia.org/w/api.php', {
-                params: {
-                  action: 'query',
-                  list: 'search',
-                  origin: '*',
-                  format: 'json',
-                  srsearch: searchText,
-                },
-        }
-            )
-            setResults(response.data.query.search)
-        }
-        if(searchText && ! results.length){
-            search()
-        }
-        else{
-            const timeoutId=setTimeout(() => {
-                if(searchText){
-                    search()
-                    }
-            }, 500);
+            const response = await axios.get('https://en.wikipedia.org/w/api.php', {
+                       params: {
+                         action: 'query',
+                         list: 'search',
+                         origin: '*',
+                         format: 'json',
+                         srsearch: debouncedText,
+                       },
+               }
+                   )
+                           setResults(response.data.query.search)
+               }
+               search()
+     },[debouncedText])
+
+
+    // useEffect(()=>{
+    //     const search = async ()=>{
+    //  const response = await axios.get('https://en.wikipedia.org/w/api.php', {
+    //             params: {
+    //               action: 'query',
+    //               list: 'search',
+    //               origin: '*',
+    //               format: 'json',
+    //               srsearch: searchText,
+    //             },
+    //     }
+    //         )
+    //         setResults(response.data.query.search)
+    //     }
+    //     if(searchText && ! results.length){
+    //         search()
+    //     }
+    //     else{
+    //         const timeoutId=setTimeout(() => {
+    //             if(searchText){
+    //                 search()
+    //                 }
+    //         }, 500);
        
      
 
-        return ()=>{
-            clearTimeout(timeoutId)//we are cancelling the timer until the user stop typing for 500ms
-        }
-    } 
-    },[searchText])
+    //     return ()=>{
+    //         clearTimeout(timeoutId)//we are cancelling the timer until the user stop typing for 500ms
+    //     }
+    // } 
+    // },[searchText,results.length])
+ 
+
+
 
    const onTextChange=(event)=>{
         setSearchText(event.target.value)
