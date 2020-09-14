c02482f10276c6242c4aafae746e9ab3ed858caf
pranav khapra <pranavkhapra23@gmail.com>
2020-09-14 13:59:08 +0530

    the wikipedia type

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React from 'react'
-import Accordion from './components/Accordion'
-
-const items=[
-  { 
-    id:1,
-    title:'What is React',
-    content:'React is a front end javascript library'
-  },
-  {
-    id:2,
-    title:'What is React',
-    content:' Suspendisse eleifend elit vel hendrerit posuere. Integer ac libero eget sapien aliquam hendrerit. Donec imperdiet eu ipsum eu rhoncus. Curabitur imperdiet, nulla sed sollicitudin ullamcorper, ex lorem imperdiet tortor, nec sollicitudin dui magna nec risus. Curabitur rutrum, lorem in efficitur egestas, quam tellus aliquet dolor, a faucibus justo neque vitae erat. Mauris eget sodales turpis. Sed rutrum nulla eget urna cursus, vel ullamcorper ipsum cursus. Duis quis leo iaculis, semper mauris eu, vulputate mauris. Maecenas maximus sem ut pulvinar commodo. Sed fringilla nunc sapien, venenatis varius purus lacinia id.  '
-  },
-  {
-    id:3,
-    title:'What is React',
-  content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam laoreet ac libero id egestas. In vitae purus tempor, hendrerit magna id, elementum ligula. Aliquam cursus a velit posuere imperdiet. Phasellus vel sollicitudin felis. Mauris venenatis vehicula ante, ac scelerisque magna egestas mollis. Nulla sit amet ipsum aliquam, ultricies dolor at, egestas sem. Vivamus ornare aliquam turpis in sagittis. Suspendisse cursus massa vitae urna vehicula, in pharetra lectus cursus. Maecenas a pharetra nisi, vitae feugiat quam. Pellentesque ac felis nec ex tristique vehicula quis in mi. Morbi vitae justo aliquam, fringilla nisi at, porttitor est. Sed arcu turpis, bibendum faucibus euismod nec, condimentum in sapien. Duis tristique elit nec mi accumsan volutpat. Proin pharetra leo eu tellus eleifend eleifend. Duis bibendum ante eget nulla tempus sodales. '
-}]
+// import Accordion from './components/Accordion'
+import Search from './components/Search'
+// const items=[
+//   { 
+//     id:1,
+//     title:'What is React',
+//     content:'React is a front end javascript library'
+//   },
+//   {
+//     id:2,
+//     title:'What is React',
+//     content:' Suspendisse eleifend elit vel hendrerit posuere. Integer ac libero eget sapien aliquam hendrerit. Donec imperdiet eu ipsum eu rhoncus. Curabitur imperdiet, nulla sed sollicitudin ullamcorper, ex lorem imperdiet tortor, nec sollicitudin dui magna nec risus. Curabitur rutrum, lorem in efficitur egestas, quam tellus aliquet dolor, a faucibus justo neque vitae erat. Mauris eget sodales turpis. Sed rutrum nulla eget urna cursus, vel ullamcorper ipsum cursus. Duis quis leo iaculis, semper mauris eu, vulputate mauris. Maecenas maximus sem ut pulvinar commodo. Sed fringilla nunc sapien, venenatis varius purus lacinia id.  '
+//   },
+//   {
+//     id:3,
+//     title:'What is React',
+//   content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam laoreet ac libero id egestas. In vitae purus tempor, hendrerit magna id, elementum ligula. Aliquam cursus a velit posuere imperdiet. Phasellus vel sollicitudin felis. Mauris venenatis vehicula ante, ac scelerisque magna egestas mollis. Nulla sit amet ipsum aliquam, ultricies dolor at, egestas sem. Vivamus ornare aliquam turpis in sagittis. Suspendisse cursus massa vitae urna vehicula, in pharetra lectus cursus. Maecenas a pharetra nisi, vitae feugiat quam. Pellentesque ac felis nec ex tristique vehicula quis in mi. Morbi vitae justo aliquam, fringilla nisi at, porttitor est. Sed arcu turpis, bibendum faucibus euismod nec, condimentum in sapien. Duis tristique elit nec mi accumsan volutpat. Proin pharetra leo eu tellus eleifend eleifend. Duis bibendum ante eget nulla tempus sodales. '
+// }]
 
 function App() {
   return (
     <div>
-      <Accordion items={items}/>
+      {/* <Accordion items={items}/> */}
+      <Search/>
     </div>
   )
 }
---
+++ src/components/Search.js
@@ -0,0 +1,80 @@
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
+function Search() {
+    const [searchText,setSearchText]= useState('user')
+    const [results,setResults]=useState([])
+    useEffect(()=>{
+        const search = async ()=>{
+     const response = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                  action: 'query',
+                  list: 'search',
+                  origin: '*',
+                  format: 'json',
+                  srsearch: searchText,
+                },
+        }
+            )
+            setResults(response.data.query.search)
+        }
+        if(searchText && ! results.length){
+            search()
+        }
+        else{
+            const timeoutId=setTimeout(() => {
+                if(searchText){
+                    search()
+                    }
+            }, 500);
+       
+     
+
+        return ()=>{
+            clearTimeout(timeoutId)//we are cancelling the timer until the user stop typing for 500ms
+        }
+    } 
+    },[searchText])
+
+   const onTextChange=(event)=>{
+        setSearchText(event.target.value)
+   }
+
+    const renderedResults=results.map((result)=>{
+      return ( <div key={result.pageid} className='item'>
+           <div className='right floated content'>
+               <a 
+               href={`https://en.wikipedia.org?curid=${result.pageid}`}
+               className='ui button'>Go</a>
+           </div>
+            <div className='content'>
+               <div className='header'>
+                   {result.title}
+               </div>
+               <span dangerouslySetInnerHTML={{
+                   __html:result.snippet
+               }}></span>
+            </div>
+        </div>
+      )
+    })
+    return (
+        <div>
+        <div className='ui form'>
+            <div className='field'>
+            <label>Enter Search Text</label>
+            <input 
+            className='input'
+            type='text'
+            value={searchText}
+            onChange={onTextChange}
+            />
+            </div>
+            </div>
+            <div className='ui celled list'>
+                {renderedResults}
+            </div>
+        </div>
+    )
+}
+
+export default Search
